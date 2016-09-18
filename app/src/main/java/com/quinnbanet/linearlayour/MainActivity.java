69bbea4dd5ba2752e8cb06417140f3db366fe516
Quinn Banet <quinnbanet359@gmail.com>
2016-09-18 15:52:22 -0400

    Logcat Works
    user input is now stored in log.d tag “Submit”

--- app/src/main/java/com/quinnbanet/linearlayour/MainActivity.java
+++ app/src/main/java/com/quinnbanet/linearlayour/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    EditText name;
-    EditText city;
-    EditText state;
-    EditText zip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +23,33 @@ public class MainActivity extends AppCompatActivity {
     public void clickEvent(View view) {
         Toast submitToast = Toast.makeText(this, "Data Submitted", Toast.LENGTH_SHORT);
         submitToast.show();
-        String logger = String.format("Name: , City: , State: , Zip: ",
+
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText city = (EditText) findViewById(R.id.city);
+        EditText state = (EditText) findViewById(R.id.state);
+        EditText zip = (EditText) findViewById(R.id.zip);
+
+
+            String userInfo = String.format("Name: %1$s City: %2$s State: %3$s Zip: %4$s",
                 name.getText().toString(), city.getText().toString(),
                 state.getText().toString(), zip.getText().toString());
-        Log.d("Submit",logger);
+       Log.d("Submit",userInfo);
+        /*
+        try {
+            File file = new File("/sdcard/userInfoLogFile.txt");
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            FileWriter fileWritter = new FileWriter(file.getName(), true);
+            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+            bufferWritter.write(userInfo);
+            bufferWritter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } */
+
     }
 }
